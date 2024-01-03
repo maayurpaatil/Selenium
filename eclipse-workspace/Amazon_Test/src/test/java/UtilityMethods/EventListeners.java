a4868ab6a280a79ad02fe66e49dfc42b8c7b45d4
DESKTOP-0BHOKNG\Admin <maayur.paatil@gmail.com>
2024-01-03 23:34:39 +0530

    Author: maayurpaatil Date: Monday, December 26, 2023 7:19:22 AM Committer: mayur Initial commit

---
+++ eclipse-workspace/Amazon_Test/src/test/java/UtilityMethods/EventListeners.java
@@ -0,0 +1,13 @@
+package UtilityMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.events.WebDriverListener;
+
+public class EventListeners extends CommonMethods implements WebDriverListener {
+
+	public EventListeners(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
